1e6b55ad0c33f7cc8ebaa7c17f3a38e9288c1aa6
N!kola Igic <igic.nikola01@gmail.com>
2026-08-27 01:27:50 +0200

    mail update

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'NSTARTER_VERSION', '1.3.3' );
+define( 'NSTARTER_VERSION', '1.3.4' );
 define( 'NSTARTER_PATH', get_stylesheet_directory() );
 define( 'NSTARTER_URL', get_stylesheet_directory_uri() );
 
