cb1173a89a85b6faf20af0b8bf8ec8cf937681fe
N!kola Igic <igic.nikola01@gmail.com>
2026-08-26 23:21:50 +0200

    photos fix

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'NSTARTER_VERSION', '1.2.0' );
+define( 'NSTARTER_VERSION', '1.2.1' );
 define( 'NSTARTER_PATH', get_stylesheet_directory() );
 define( 'NSTARTER_URL', get_stylesheet_directory_uri() );
 
