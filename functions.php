f819cb48237f6eb7a8ed35271853e578200d1806
N!kola Igic <igic.nikola01@gmail.com>
2026-09-04 00:06:59 +0200

    blue change

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'NSTARTER_VERSION', '1.5.45' );
+define( 'NSTARTER_VERSION', '1.5.47' );
 define( 'NSTARTER_PATH', get_stylesheet_directory() );
 define( 'NSTARTER_URL', get_stylesheet_directory_uri() );
 
