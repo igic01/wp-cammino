2d4e9919e100f5c7ef2ad3ab4f3ad2011a9a9ff3
N!kola Igic <igic.nikola01@gmail.com>
2026-09-04 00:46:00 +0200

    color updates

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'NSTARTER_VERSION', '1.5.56' );
+define( 'NSTARTER_VERSION', '1.5.57' );
 define( 'NSTARTER_PATH', get_stylesheet_directory() );
 define( 'NSTARTER_URL', get_stylesheet_directory_uri() );
 
