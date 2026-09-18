ee21cb92c056839ac93c787c3a3f82db04f8e8ef
N!kola Igic <igic.nikola01@gmail.com>
2026-09-18 09:54:11 +0200

    removing categoies 2

--- inc/posts.php
+++ inc/posts.php
@@ -296,7 +296,7 @@ add_action( 'admin_enqueue_scripts', static function ( string $hook ): void {
 	if ( in_array( $hook, array( 'post.php', 'post-new.php' ), true ) && 'post' === get_current_screen()->post_type ) {
 		wp_enqueue_script( 'cammino-post-settings', NSTARTER_URL . '/assets/js/post-settings.js', array(), NSTARTER_VERSION, true );
 		if ( get_current_screen()->is_block_editor() ) {
-			wp_enqueue_script( 'cammino-post-categories', NSTARTER_URL . '/assets/js/post-categories.js', array( 'wp-hooks', 'wp-element', 'wp-data', 'wp-core-data', 'wp-editor', 'wp-components', 'wp-i18n', 'wp-html-entities' ), NSTARTER_VERSION, true );
+			wp_enqueue_script( 'cammino-post-categories', NSTARTER_URL . '/assets/js/post-categories.js', array( 'wp-hooks', 'wp-element', 'wp-data', 'wp-core-data', 'wp-editor', 'wp-components', 'wp-i18n', 'wp-html-entities' ), (string) filemtime( NSTARTER_PATH . '/assets/js/post-categories.js' ), true );
 			wp_localize_script( 'cammino-post-categories', 'camminoPostCategories', array( 'eventCategoryId' => cammino_get_event_category_id( false ) ) );
 		}
 	}
